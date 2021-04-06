01da033f5dcb0c8e6c03710ec41622f4da3756a7
JarSeal <kai.forsman@gmail.com>
2021-04-06 12:24:34 +0800

    Added more settings and features.

--- src/js/Root.js
+++ src/js/Root.js
@@ -22,17 +22,18 @@ class Root {
 
         // Setup scene and basic lights [START]
         const scene = new THREE.Scene();
-        const hemi = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.65);
-        hemi.position.set(32, -32, 5);
-        //scene.add(hemi);
-        scene.add(new THREE.AmbientLight(0xffffff, 0.5));
-        scene.add(new THREE.AxesHelper(10)); // Helper
+        const hemi = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.15);
+        hemi.position.set(0, 32, 0);
+        scene.add(hemi);
+        scene.add(new THREE.AmbientLight(0xffffff, 0));
+        this.axesHelper = new THREE.AxesHelper(10);
+        scene.add(this.axesHelper); // Helper
         this.scene = scene;
         // Setup scene and basic lights [/END]
 
         // Setup camera and aspect ratio [START]
         this.aspectRatio = screenSize.x / screenSize.y;
-        const camera = new THREE.PerspectiveCamera(45, this.aspectRatio, 0.1, 5000);
+        const camera = new THREE.PerspectiveCamera(45, this.aspectRatio, 0.1, 200);
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.update();
         this.controls = controls;
@@ -56,17 +57,54 @@ class Root {
         this.sceneState.resizeFns = [this.resize],
         this.sceneState.getScreenResolution = this.getScreenResolution;
         this.sceneState.defaultSettings = {
-            showStats: true
+            showStats: true,
+            showAxesHelper: true,
+            showEnvMapBackground: true,
+            sceneBackColor: '#000000',
+            useHemiLight: true,
+            hemiLightIntensity: 0.15,
+            useAmbiLight: true,
+            ambiLightIntensity: 0.5,
+            curMaterialPreviewObjects: 'SphereAndPlane2',
+            curMaterialSide: 'DoubleSide',
+            useIBL: true,
+            aoMapIntensity: 0.5,
         };
         this.sceneState.settings = { ...this.sceneState.defaultSettings };
-        this.initResizer();
+        this._setupLights();
+        this._initResizer();
         // Other setup [/END]
 
         // GUI setup [START]
         const gui = new GUI();
-        gui.close();
-        gui.add(this.sceneState.settings, 'showStats').name('Show stats').onChange((value) => {
+        // gui.close();
+        const sceneAndDebugFolder = gui.addFolder('Scene and Debug');
+        sceneAndDebugFolder.add(this.sceneState.settings, 'showStats').name('Show stats').onChange((value) => {
             document.getElementById('debug-stats-wrapper').style.display = value ? 'block' : 'none';
+        });
+        sceneAndDebugFolder.add(this.sceneState.settings, 'showAxesHelper').name('Show axes helper').onChange((value) => {
+            this.axesHelper.visible = value;
+        });
+        sceneAndDebugFolder.add(this.sceneState.settings, 'showEnvMapBackground').name('Show envmap bckgrd').onChange((value) => {
+            scene.background = value ? this.sceneState.curCubeMap : new THREE.Color(this.sceneState.settings.sceneBackColor);
+        });
+        sceneAndDebugFolder.addColor(this.sceneState.settings, 'sceneBackColor').name('Scene back color').onChange((value) => {
+            if(!this.sceneState.settings.showEnvMapBackground) {
+                scene.background = new THREE.Color(value);
+            }
+        });
+        sceneAndDebugFolder.add(this.sceneState.settings, 'useHemiLight').name('Use Hemi light').onChange((value) => {
+            this.scene.children[0].visible = value;
+        });
+        sceneAndDebugFolder.add(this.sceneState.settings, 'hemiLightIntensity', 0, 2).name('Hemi light intensity').onChange((value) => {
+            this.scene.children[0].intensity = value;
+        });
+        sceneAndDebugFolder.add(this.sceneState.settings, 'useAmbiLight').name('Use Ambi light').onChange((value) => {
+            console.log(this.scene);
+            this.scene.children[1].visible = value;
+        });
+        sceneAndDebugFolder.add(this.sceneState.settings, 'ambiLightIntensity', 0, 2).name('Ambi light intensity').onChange((value) => {
+            this.scene.children[1].intensity = value;
         });
         this.sceneState.gui = gui;
         // GUI setup [/END]
@@ -80,28 +118,11 @@ class Root {
         camera.position.set(1, 1, 10);
         camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-        // Cubemaps
-        const Cmaps = new Cubemaps(scene, this.sceneState);
-
-        // Add ground
-        // const groundGeo = new THREE.BoxBufferGeometry(5, 0.2, 2);
-        // const groundMat = new THREE.MeshLambertMaterial({ color: 0x666666 });
-        // const groundMesh = new THREE.Mesh(groundGeo, groundMat);
-        // groundMesh.position.set(0, -0.1, 0);
-        // scene.add(groundMesh);
-
-        // Add a box
-        // const boxGeo = new THREE.BoxBufferGeometry(1, 1, 1);
-        // const boxMat = new THREE.MeshLambertMaterial({ color: 0x999999 });
-        // const boxMesh = new THREE.Mesh(boxGeo, boxMat);
-        // boxMesh.position.set(0, 0.5, 0);
-        // scene.add(boxMesh);
-
-        // Main app logic [/END]
+        // Init Cubemaps
+        new Cubemaps(scene, this.sceneState);
 
         this.resize(this.sceneState, this.renderer);
         this.renderLoop();
-
     }
 
     renderLoop = () => {
@@ -124,7 +145,7 @@ class Root {
         renderer.setPixelRatio(pixelRatio);
     }
 
-    initResizer() {
+    _initResizer() {
         let resizeTimer;
         window.addEventListener('resize', () => {
             clearTimeout(resizeTimer);
@@ -149,6 +170,13 @@ class Root {
             x: Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
             y: Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
         };
+    }
+
+    _setupLights() {
+        this.scene.children[0].visible = this.sceneState.settings.useHemiLight;
+        this.scene.children[0].intensity = this.sceneState.settings.hemiLightIntensity;
+        this.scene.children[1].visible = this.sceneState.settings.useAmbiLight;
+        this.scene.children[1].intensity = this.sceneState.settings.ambiLightIntensity;
     }
 }
 
--- src/js/cubemaps/Cubemaps.js
+++ src/js/cubemaps/Cubemaps.js
@@ -4,49 +4,117 @@ class Cubemaps {
     constructor(scene, sceneState) {
         this.sceneState = sceneState;
         this.loader = new THREE.CubeTextureLoader();
-        
-        // const boxGeo = new THREE.BoxBufferGeometry(1, 1, 1);
-        // const boxMat = new THREE.MeshLambertMaterial({ color: 0x999999 });
-        // const boxMesh = new THREE.Mesh(boxGeo, boxMat);
-        // boxMesh.position.set(0, 0.5, 0);
-        // scene.add(boxMesh);
 
-        const defaultMapPath = '/images/cubemaps/LancelottiChapel/';
-        scene.background = this.loadCubemaps(defaultMapPath);
+        // const defaultMapPath = '/images/cubemaps/LancelottiChapel/';
 
-        this.loadSphere(scene, [0, 0, 0], defaultMapPath);
+        this._loadObjects(scene, [0, 0, 0]);
+        this._createGui(sceneState);
     }
 
-    loadCubemaps(path) {
-        const urls = [
-            path+'posx_256.jpg', path+'negx_256.jpg', path+'posy_256.jpg', path+'negy_256.jpg', path+'posz_256.jpg', path+'negz_256.jpg'
-        ];
+    _loadCubemaps(path) {
+        // const urls = [ path+'posx_256.jpg', path+'negx_256.jpg', path+'posy_256.jpg', path+'negy_256.jpg', path+'posz_256.jpg', path+'negz_256.jpg' ];
+        const urls = [ path+'posx.jpg', path+'negx.jpg', path+'posy.jpg', path+'negy.jpg', path+'posz.jpg', path+'negz.jpg' ];
         return this.loader.load(urls);
     }
 
-    loadSphere(scene, pos, path) {
-        const urls = [
-            path+'posx_256.jpg', path+'negx_256.jpg', path+'posy_256.jpg', path+'negy_256.jpg', path+'posz_256.jpg', path+'negz_256.jpg'
-        ];
+    _loadObjects(scene, pos) {
+        const path = '/images/cubemaps/Teide/';
+        const urls = [ path+'posx.jpg', path+'negx.jpg', path+'posy.jpg', path+'negy.jpg', path+'posz.jpg', path+'negz.jpg' ];
         this.loader.load(urls, (cubeMap) => {
+            this.sceneState.curCubeMap = cubeMap;
+            if(this.sceneState.settings.showEnvMapBackground) {
+                scene.background = cubeMap;
+            }
             const blur = new THREE.PMREMGenerator(this.sceneState.renderer);
-            const test = blur.fromCubemap(cubeMap);
-            console.log(blur, test, cubeMap);
-            const sphereMat = new THREE.MeshStandardMaterial({
+            const ibl = blur.fromCubemap(cubeMap);
+            this.sceneState.curIBL = ibl;
+            const mat = new THREE.MeshStandardMaterial({
                 map: new THREE.TextureLoader().load('/images/textures/concrete2/concrete2_albedo_512.png'),
                 normalMap: new THREE.TextureLoader().load('/images/textures/concrete2/concrete2_Normal-ogl_512.png'),
-                envMap: test.texture,
-                metalness: 0.2,
+                envMap: this.sceneState.settings.useIBL ? ibl.texture : null,
+                metalness: 0.15,
                 // metalnessMap: new THREE.TextureLoader().load('/images/textures/concrete2/concrete2_Metallic.png'),
                 // roughnessMap: new THREE.TextureLoader().load('/images/textures/concrete2/concrete2_Roughness.png'),
-                roughness: 0.2,
+                roughness: 0.45,
+                side: this.sceneState.settings.curMaterialSide === 'FrontSide'
+                    ? THREE.FrontSide
+                    : this.sceneState.settings.curMaterialSide === 'BackSide'
+                        ? THREE.BackSide
+                        : THREE.DoubleSide,
                 aoMap: new THREE.TextureLoader().load('/images/textures/concrete2/concrete2-ao_512.png'),
+                aoMapIntensity: this.sceneState.settings.aoMapIntensity,
             });
+
+            const curSetting = this.sceneState.settings.curMaterialPreviewObjects;
+
+            const boxGeo = new THREE.BoxBufferGeometry(1, 1, 1);
+            const boxMesh = new THREE.Mesh(boxGeo, mat);
+            boxMesh.position.set(pos[0], pos[1], pos[2]);
+            boxMesh.visible = curSetting === 'Box' || curSetting === 'BoxAndPlane1' || curSetting === 'BoxAndPlane2';
+            scene.add(boxMesh);
+
             const sphereGeo = new THREE.SphereGeometry(1, 32, 32);
-            const sphere = new THREE.Mesh(sphereGeo, sphereMat);
-            sphere.position.set(pos[0], pos[1], pos[2]);
-            scene.add(sphere);
-            sphere.material.needsUpdate = true;
+            const sphereMesh = new THREE.Mesh(sphereGeo, mat);
+            sphereMesh.position.set(pos[0], pos[1], pos[2]);
+            sphereMesh.visible = curSetting === 'Sphere' || curSetting === 'SphereAndPlane1' || curSetting === 'SphereAndPlane2';
+            scene.add(sphereMesh);
+
+            const planeGeo = new THREE.PlaneBufferGeometry(5, 5, 1);
+            const planeMesh = new THREE.Mesh(planeGeo, mat);
+            planeMesh.position.set(pos[0], pos[1], pos[2]);
+            if(curSetting === 'Plane1' || curSetting === 'BoxAndPlane1' || curSetting === 'SphereAndPlane1') {
+                planeMesh.rotation.x = Math.PI / -2;
+            }
+            planeMesh.visible = curSetting === 'Plane1' || curSetting === 'Plane2' || curSetting === 'BoxAndPlane1' || curSetting === 'BoxAndPlane2' || curSetting === 'SphereAndPlane1' || curSetting === 'SphereAndPlane2';
+            scene.add(planeMesh);
+
+            this.sceneState.matPrevObjects = {
+                box: boxMesh,
+                sphere: sphereMesh,
+                plane: planeMesh,
+            };
+            this.sceneState.curMat = mat;
+        });
+    }
+
+    _createGui(sceneState) {
+        const objectsFolder = sceneState.gui.addFolder('Object(s)');
+        objectsFolder.add(this.sceneState.settings, 'curMaterialPreviewObjects', [ 'None', 'Box', 'Sphere', 'Plane1', 'Plane2', 'BoxAndPlane1', 'BoxAndPlane2', 'SphereAndPlane1', 'SphereAndPlane2' ]).name('Mat prev object(s)').onChange((value) => {
+            this.sceneState.matPrevObjects.box.visible = value === 'Box' || value === 'BoxAndPlane1' || value === 'BoxAndPlane2';
+            this.sceneState.matPrevObjects.sphere.visible = value === 'Sphere' || value === 'SphereAndPlane1' || value === 'SphereAndPlane2';
+            this.sceneState.matPrevObjects.plane.visible = value === 'Plane1' || value === 'Plane2' || value === 'BoxAndPlane1' || value === 'BoxAndPlane2' || value === 'SphereAndPlane1' || value === 'SphereAndPlane2';
+            if(value === 'Plane1' ||  value === 'BoxAndPlane1' || value === 'SphereAndPlane1') {
+                this.sceneState.matPrevObjects.plane.rotation.x = Math.PI / -2;
+            } else {
+                this.sceneState.matPrevObjects.plane.rotation.x = 0;
+            }
+        });
+        objectsFolder.add(this.sceneState.settings, 'curMaterialSide', [ 'FrontSide', 'BackSide', 'DoubleSide' ]).name('Mat side rendering').onChange((value) => {
+            const mat = this.sceneState.curMat;
+            mat.side = value === 'FrontSide'
+                ? THREE.FrontSide
+                : value === 'BackSide'
+                    ? THREE.BackSide
+                    : THREE.DoubleSide;
+            mat.needsUpdate = true;
+        });
+
+        const envMapFolder = sceneState.gui.addFolder('Env Map');
+        envMapFolder.open();
+        envMapFolder.add(this.sceneState.settings, 'useIBL').name('Use IBL').onChange((value) => {
+            const mat = this.sceneState.curMat;
+            if(value) {
+                mat.envMap = this.sceneState.curIBL.texture;
+            } else {
+                mat.envMap = null;
+            }
+        });
+
+        const materialFolder = sceneState.gui.addFolder('Material');
+        materialFolder.add(this.sceneState.settings, 'aoMapIntensity', 0, 1).name('AO map intensity').onChange((value) => {
+            const mat = this.sceneState.curMat;
+            mat.aoMapIntensity = value;
+            mat.needsUpdate = true;
         });
     }
 }
